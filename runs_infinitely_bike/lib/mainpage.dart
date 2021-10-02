058ae18214f84f5b8a0a730257d0bc30c40653b1
yuto90 <solidusgako4087@gmail.com>
2021-10-02 21:19:24 +0900

    風景を立体的に表現

--- runs_infinitely_bike/lib/mainpage.dart
+++ runs_infinitely_bike/lib/mainpage.dart
@@ -13,31 +13,40 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPage extends State<MainPage> {
-  static double bikeYaxis = 1.1;
+  static double bikeY = 0.6;
   double time = 0;
-  double initialHeight = bikeYaxis;
+  double initialHeight = bikeY;
   bool gameHasStarted = false;
 
+  // 背景
   static double desertOneX = -1;
-  static double desertTwoX = 3.1;
-  static double desertThreeX = 7.2;
+  static double desertTwoX = 3.7;
+  static double desertThreeX = 8.4;
 
-  double sabotenFlowerYaxis = 1;
+  // サボテンフラワー
+  double sabotenFlowerY = 1;
   static double sabotenFlowerOneX = 1;
 
-  double yasiYaxis = 1;
+  // ヤシの木
+  double yasiOneY = -0.2;
+  double yasiTwoY = 1;
+  double yasiThreeY = -0.2;
+  double yasiFourY = 1;
   static double yasiOneX = 1;
   static double yasiTwoX = 1.5;
+  static double yasiThreeX = 1.5;
+  static double yasiFourX = 2;
 
   bool sindou = true;
 
+  // ODOメーター
   int odo = 0;
   int milli = 0;
 
   void move() {
     setState(() {
       //time = 0;
-      //initialHeight = bikeYaxis;
+      //initialHeight = bikeY;
     });
   }
 
@@ -46,34 +55,34 @@ class _MainPage extends State<MainPage> {
     Timer.periodic(Duration(milliseconds: 50), (timer) {
       if (sindou) {
         setState(() {
-          bikeYaxis = initialHeight + 0.005;
+          bikeY = initialHeight + 0.005;
         });
       } else {
         setState(() {
-          bikeYaxis = initialHeight - 0.005;
+          bikeY = initialHeight - 0.005;
         });
       }
       sindou = !sindou;
 
       setState(() {
-        if (desertOneX < -5.3) {
-          desertOneX += 10.6;
+        if (desertOneX < -6) {
+          desertOneX += 12;
         } else {
           desertOneX -= 0.01;
         }
       });
 
       setState(() {
-        if (desertTwoX < -5.3) {
-          desertTwoX += 10.6;
+        if (desertTwoX < -6) {
+          desertTwoX += 12;
         } else {
           desertTwoX -= 0.01;
         }
       });
 
       setState(() {
-        if (desertThreeX < -5.3) {
-          desertThreeX += 10.6;
+        if (desertThreeX < -6) {
+          desertThreeX += 12;
         } else {
           desertThreeX -= 0.01;
         }
@@ -103,10 +112,26 @@ class _MainPage extends State<MainPage> {
         }
       });
 
-      // odoメーター（1秒+=1km)
+      setState(() {
+        if (yasiThreeX < -2) {
+          yasiThreeX += 4;
+        } else {
+          yasiThreeX -= 0.005;
+        }
+      });
+
+      setState(() {
+        if (yasiFourX < -2) {
+          yasiFourX += 4;
+        } else {
+          yasiFourX -= 0.005;
+        }
+      });
+
+      // odoメーター（5秒+=1km)
       setState(() {
         milli += 50;
-        if (milli % 1000 == 0) {
+        if (milli % 5000 == 0) {
           odo += 1;
         }
       });
@@ -124,59 +149,75 @@ class _MainPage extends State<MainPage> {
         }
       },
       child: Scaffold(
-        body: Column(
+        body: Stack(
           children: [
-            Expanded(
-              flex: 5,
-              child: Stack(
-                children: [
-                  Container(
-                    color: Colors.indigo,
-                  ),
-                  Container(
-                    alignment: Alignment(desertOneX, 1.1),
-                    child: Desert(),
-                  ),
-                  Container(
-                    alignment: Alignment(desertTwoX, 1.1),
-                    child: Desert(),
-                  ),
-                  Container(
-                    alignment: Alignment(desertThreeX, 1.1),
-                    child: Desert(),
-                  ),
-                  Container(
-                    alignment: Alignment(sabotenFlowerOneX, sabotenFlowerYaxis),
-                    child: SabotenFlower(),
-                  ),
-                  Container(
-                    alignment: Alignment(yasiOneX, yasiYaxis),
-                    child: Yasi(pattern: 1),
-                  ),
-                  Container(
-                    alignment: Alignment(yasiTwoX, yasiYaxis),
-                    child: Yasi(pattern: 2),
-                  ),
-                  Container(
-                    alignment: Alignment(-0.5, bikeYaxis),
-                    child: MyBike(),
-                  ),
-                  Container(
-                    alignment: Alignment(0, 0.2),
-                    child: gameHasStarted
-                        ? Text('')
-                        : Text(
-                            'Infinit Bike \nT A P  T O  P L A Y',
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                  ),
-                ],
+            Container(
+              color: Colors.indigo,
+            ),
+            Container(
+              alignment: Alignment(desertOneX, 1),
+              child: Desert(),
+            ),
+            Container(
+              alignment: Alignment(desertTwoX, 1),
+              child: Desert(),
+            ),
+            Container(
+              alignment: Alignment(desertThreeX, 1),
+              child: Desert(),
+            ),
+            Container(
+              alignment: Alignment(sabotenFlowerOneX, sabotenFlowerY),
+              child: SabotenFlower(),
+            ),
+            // ヤシの木 1
+            Container(
+              alignment: Alignment(yasiOneX, yasiOneY),
+              child: Yasi(pattern: 1),
+            ),
+            // ヤシの木 1
+            Container(
+              alignment: Alignment(yasiThreeX, yasiThreeY),
+              child: Yasi(pattern: 2),
+            ),
+            // 道路
+            Positioned(
+              bottom: 45,
+              child: Container(
+                height: 70,
+                width: 2000,
+                color: Colors.grey,
               ),
             ),
-            Expanded(
-              flex: 1,
+
+            // バイク
+            Container(
+              alignment: Alignment(-0.5, bikeY),
+              child: MyBike(),
+            ),
+            // ヤシの木 2
+            Container(
+              alignment: Alignment(yasiTwoX, yasiTwoY),
+              child: Yasi(pattern: 2),
+            ),
+            // ヤシの木 2
+            Container(
+              alignment: Alignment(yasiFourX, yasiFourY),
+              child: Yasi(pattern: 1),
+            ),
+            Container(
+              alignment: Alignment(0, 0.2),
+              child: gameHasStarted
+                  ? Text('')
+                  : Text(
+                      'Infinit Bike \nT A P  T O  P L A Y',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+            ),
+            Positioned(
+              top: 10,
+              left: 20,
               child: Container(
-                color: Colors.grey,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
@@ -192,7 +233,7 @@ class _MainPage extends State<MainPage> {
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
